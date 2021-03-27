ae951f5b97ee7c15bfc84027465e4d9a318394fb
Felipe Guimaraes <felipeguima4@gmail.com>
2021-03-26 22:52:42 -0300

    [+]Adds project's skeleton

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@
 int main(int argc, char const *argv[]){
 
     if(argc> 1){
-        
+        CPU *Cpu = new CPU(argv[1]);
+        Cpu->run();
+        Cpu->print();
+        delete Cpu;
     }else{
         std::cout << "No input file provided" << std::endl;
     }
